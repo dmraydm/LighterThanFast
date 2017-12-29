c6f916b9d969abe3aa4261582f3e5e86ae4b47f3
egaouyer@gmail.com <egaouyer@gmail.com>
2017-12-29 21:04:20 +0100

    Villages naming

---
+++ Defs/ThingDefs_Items/patchVanillaForRecipes.xml.d
@@ -0,0 +1,31 @@
+<!--
+<?xml version="1.0" encoding="utf-8" ?>
+<Defs>
+	<Patch>
+	<Operation Class="PatchOperationSequence">
+	  <success>Always</success>
+	  
+	  <operations>
+		<li Class="PatchOperationTest">
+		  <xpath>*/ThingDef[defName = "Stool"]/comps</xpath>
+		  <success>Invert</success>
+		</li>
+		
+		<li Class="PatchOperationInsert">
+		  <xpath>*/ThingDef[defName = "Stool"]</xpath>
+		  <value>
+			<li Class="CompProperties_Ingredients" />
+		  </value>
+		</li>
+	  </operations>
+	  
+	</Operation>
+	</Patch>
+</Defs>
+-->
+	<!-- <Operation Class="PatchOperationInsert"> -->
+	  <!-- <xpath>*/DesignationCategoryDef[defName = "ExampleCategory"]/specialDesignatorClasses</xpath> -->
+	  <!-- <value> -->
+        <!-- <description>Pointless example designators.</description> -->
+	  <!-- </value> -->
+	<!-- </Operation>  -->
